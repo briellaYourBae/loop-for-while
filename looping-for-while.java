d82a3585315db18ab77ff017639e04dda30446aa
BrieLLa <117743500+briellaYourBae@users.noreply.github.com>
2023-08-31 22:11:35 +0700

    Create looping-for-while.java

---
+++ looping-for-while.java
@@ -0,0 +1,27 @@
+/**
+ *
+ * @author @briella.yb
+ */
+class LoopingFor{
+    public static void main(String[] args){
+
+        for(int i=0; i <= 5; i++){
+            System.out.println("Aku sayang kamuu");
+        }
+    }
+}
+
+//buat class file baru untuk mengakses.
+//ini 2 file tapi saya jadikan 1 saja, class atas untuk looping for dan bawah untuk looping while!
+
+public class LoopingWhile {
+  public static void main(String[] args) {
+    int count = 1;
+    do {
+      System.out.println("nomer : " + count);
+      count++;
+    }
+    while (count < 5);  
+    System.out.println("udaah gtuu aja yaa aku capee tauu");
+  }
+}
